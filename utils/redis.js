936cd7720b8793a64366e376ba397ebad770cdb8
Iheb Chaoued <ihebchaoued@gmail.com>
2024-06-29 23:54:58 +0000

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,42 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.error('Redis Client Error', err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) return reject(err);
+        resolve(value);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) return reject(err);
+        resolve(true);
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) return reject(err);
+        resolve(true);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
